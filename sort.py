4200e5b1a8f92e7bf960da04797abcfb309b7a36
Victor Guerra <wasmon999@hotmail.com>
2019-09-07 19:52:26 -0600

    Parse lines

--- sort.py
+++ sort.py
@@ -1,13 +1,15 @@
-def shakesort(lis, begin, end):
+def shakesort(lis, scores, begin, end):
     if begin >= end:
         return
     #
     for i in range(begin, end - 1):
         if lis[i] > lis[i + 1]: 
             lis[i], lis[i + 1] = lis[i + 1], lis[i]
+            scores[i], scores[i + 1] = scores[i + 1], scores[i]
     #
     for k in range(end - 1, begin, -1):
         if lis[k] < lis[k - 1]:
             lis[k], lis[k-1] = lis[k-1],lis[k]
+            scores[k], scores[k - 1] = scores[k - 1], scores[k]
     #
-    return shakesort(lis, begin+1, end-1)
+    return shakesort(lis, scores, begin+1, end-1)
--- surf.py
+++ surf.py
@@ -1,13 +1,31 @@
 from sort import shakesort
+import re
 from sys import argv
 script, filename = argv
 
+def impr(li):
+    for i in li:
+        print(i,end=',')
+    print('\n')
+# Para imprimir una lista bonita
+#
+
 with open(filename, 'r+') as file:
     contents = file.read()
-
 lines = contents.split('\n')
-li = [7,10,4,1,8,6,9,5,2,3]
+# Extrae los contenidos del archivo txt y crea una lista
+# con cada linea.
+#li = [7,10,4,1,8,6,9,5,2,3]
 
-print(li)
-shakesort(li, 0, len(li))
-print(li)
+exp = re.compile(r"^\D+")
+# Crea una expresion regular para extraer unicamente 
+# la puntuacion de cada linea
+
+scor = []
+for line in lines:
+    rm = exp.match(line)
+    scor.append(int(line.strip(rm.group(0))))
+
+impr(lines)
+shakesort(scor, lines,0, len(lines))
+impr(lines)
